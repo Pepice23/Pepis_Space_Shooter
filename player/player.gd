f1603138b4dbf07aea915b9a9b0908456085a65a
Molnár József <mojo12984@gmail.com>
2023-10-03 10:07:37 +0200

    added ability to the player to shoot

--- player/player.gd
+++ player/player.gd
@@ -2,15 +2,21 @@ extends RigidBody2D
 
 @export var engine_power = 500
 @export var spin_power = 8000
+@export var bullet_scene : PackedScene
+@export var fire_rate = 0.25
 
 var thrust = Vector2.ZERO
 var rotation_dir = 0
+var screensize = Vector2.ZERO
+var can_shoot = true
 
 enum {INIT,ALIVE,INVULNERABLE,DEAD}
 var state = INIT
 
 func _ready():
+	screensize = get_viewport_rect().size
 	change_state(ALIVE)
+	$GunCooldown.wait_time = fire_rate
 
 func change_state(new_state):
 	match new_state:
@@ -33,11 +39,29 @@ func get_input():
 		return
 	if Input.is_action_pressed("thrust"):
 		thrust = transform.x * engine_power
+	if Input.is_action_pressed("shoot") and can_shoot:
+		shoot()
 	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
 
 func _physics_process(_delta):
 	constant_force = thrust
 	constant_torque = rotation_dir * spin_power
 
-
+func _integrate_forces(physics_state):
+	var xform = physics_state.transform
+	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
+	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
+	physics_state.transform = xform
 	
+func shoot():
+	if state == INVULNERABLE:
+		return
+	can_shoot = false
+	$GunCooldown.start()
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start($Muzzle.global_transform)
+
+
+func _on_gun_cooldown_timeout():
+	can_shoot = true
